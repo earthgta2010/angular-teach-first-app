44b6fa6bcd295513241b982e1f40aef941c8cb09
earthgta2010 <earthgta2010@gmail.com>
2019-02-10 14:34:04 +0700

    'SecondClass'

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Card } from '../card';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  @Input() card: Card;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  upVote(){
+    this.card.votes +=1;
+  }
+  downVote(){
+    this.card.votes -=1;
+  }
+}
